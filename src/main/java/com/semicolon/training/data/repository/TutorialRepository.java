c3f2017ab909397717f40f557485752a9689e359
Adeola Oladeji <deolaoladeji@gmail.com>
2022-08-12 16:26:08 +0100

    updates

--- src/main/java/com/semicolon/training/data/repository/TutorialRepository.java
+++ src/main/java/com/semicolon/training/data/repository/TutorialRepository.java
@@ -1,4 +1,9 @@
 package com.semicolon.training.data.repository;
 
-public class TutorialRepository {
+import com.semicolon.training.data.models.Tutorial;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
 }
